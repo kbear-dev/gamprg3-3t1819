85450ba2a79a90ad25edd1bd5de39d205fd6dc3b
kbear-dev <kyleberran@gmail.com>
2019-07-17 19:56:09 +0800

    Edited DeckHUD

--- Prototypes/UnitPrototype/Assets/Scripts/Systems/HUD/DeckHUD.cs
+++ Prototypes/UnitPrototype/Assets/Scripts/Systems/HUD/DeckHUD.cs
@@ -5,17 +5,50 @@ using UnityEngine.UI;
 
 public class DeckHUD : MonoBehaviour
 {
+    public RectTransform selector;
     public List<RectTransform> slots;
+
+    public Sprite EmptyCardSprite;
+
+    private int selectedSlot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        selectedSlot = 0;
+        selector.position = slots[selectedSlot].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeSelectorPosition();
+    }
+
+    private void ChangeSelectorPosition ()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            if (selectedSlot == slots.Count - 1)
+            {
+                selectedSlot = 0;
+                return;
+            }
+
+            selectedSlot++;
+            
+        }
+        else if (Input.mouseScrollDelta.y < 0)
+        {
+           if (selectedSlot == 0)
+            {
+                selectedSlot = slots.Count - 1;
+                return;
+            }
+
+            selectedSlot--;
+        }
+
+        selector.position = slots[selectedSlot].position;
     }
 }
