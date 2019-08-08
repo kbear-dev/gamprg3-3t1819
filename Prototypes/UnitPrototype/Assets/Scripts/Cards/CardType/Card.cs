5f44f16ea842eacd37d74b5c575cfeb900c71084
kbear-dev <kyleberran@gmail.com>
2019-08-08 11:52:57 +0800

    Added CardName and CardDescription to Card.cs Edited DeckHUD for CardName and CardDescription

--- Prototypes/UnitPrototype/Assets/Scripts/Cards/CardType/Card.cs
+++ Prototypes/UnitPrototype/Assets/Scripts/Cards/CardType/Card.cs
@@ -7,6 +7,10 @@ public abstract class Card : MonoBehaviour
     [HideInInspector] public Unit Caster;
     [HideInInspector] public Unit Target;
 
+    public string CardName;
+    [TextArea(minLines:1,maxLines:2)]
+    public string CardDescription;
+
     public virtual void OnThrow()
     {
         StartCoroutine(OnEffect());
--- Prototypes/UnitPrototype/Assets/Scripts/Systems/HUD/DeckHUD.cs
+++ Prototypes/UnitPrototype/Assets/Scripts/Systems/HUD/DeckHUD.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class DeckHUD : MonoBehaviour
 {
     public Image slot;
+    public Text CardName;
+    public Text CardDescription;
+
     public Deck deck;
 
     public Sprite EmptyCardSprite;
@@ -27,9 +30,15 @@ public class DeckHUD : MonoBehaviour
         if (currentCard == null)
         {
             slot.sprite = EmptyCardSprite;
+            slot.color = Color.white;
+            CardName.text = "";
+            CardDescription.text = "";
             return;
         }
 
         slot.sprite = currentCard.GetComponent<SpriteRenderer>().sprite;
+        slot.color = currentCard.GetComponent<SpriteRenderer>().color;
+        CardName.text = currentCard.CardName;
+        CardDescription.text = currentCard.CardDescription;
     }
 }
